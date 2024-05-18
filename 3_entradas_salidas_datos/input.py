0b137fdc65c1b7dcefc3079d5392028ac5376857
Marco G <169110838+VedicInvader@users.noreply.github.com>
2024-05-18 12:52:03 -0600

    Add files via upload

---
+++ 3_entradas_salidas_datos/input.py
@@ -0,0 +1,10 @@
+#   Utilizar la entrada y salida de datos en Python
+
+nombre = input("Ingrese el nombre completo del alumno: ");
+edad = input("Ingrese edad: ")
+estatura = input("Ingrese estatura: ")
+
+print("\n.:: DATOS DEL ALUMNO ::.\n") # \t es para poner una tabulacion
+print(f"Alumno: {nombre}\n")
+print(f"Edad: {edad}\n")
+print(f"Estatura: {estatura}\n")
