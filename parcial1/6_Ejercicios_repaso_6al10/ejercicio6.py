c7bbdae7f780d28e2b96e329d5b2b93778f644f6
Marco G <169110838+VedicInvader@users.noreply.github.com>
2024-05-20 11:41:07 -0600

    Delete parcial1/6_Ejercicios_repaso_6al10 directory

--- parcial1/6_Ejercicios_repaso_6al10/ejercicio6.py
+++
@@ -1,7 +0,0 @@
-#   Mostrar todas las tablas del 1 al 10. Mostrando el titulo de la tabla y luego las multiplicaciones del 1 al 10
-
-mult = 0
-for contador1 in range(1, 11):
-    for contador2 in range(1, 11):
-        mult = contador1 * contador2
-        print(f"{contador1} x {contador2} = {mult}")
--- parcial1/6_Ejercicios_repaso_6al10/ejercicio7.py
+++
@@ -1,9 +0,0 @@
-#   Hacer un programa que muestre todos los numeros impares entre 2 numeros que decida el usuario
-
-print("Mostrar numeros impares entre dos numeros que decida el usuario")
-rango1 = int(input("Ingrese el valor del rango inferior: "))
-rango2 = int(input("Ingrese el valor del rango superior: "))
-
-for contador in range(rango1 + 1, rango2):
-    if contador % 2 != 0:
-        print(contador)
