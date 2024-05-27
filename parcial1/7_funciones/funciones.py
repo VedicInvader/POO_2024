cbf678aadd7985f96db60ba1094adf5be1a3dcc9
VedicInvader <169110838+VedicInvader@users.noreply.github.com>
2024-05-27 11:13:44 -0600

    commit

---
+++ parcial1/7_funciones/funciones.py
@@ -0,0 +1,16 @@
+"""
+    Una función es un conjunto de instrucciones agrupadas bajo un nombre en particular, como un programa más pequeño que cumple
+    una función específica. La función se puede reutilizar con el simple hecho de invocarla, es decir, mandarla llamar.
+
+    Sintaxis:
+
+    def nombreFuncion (parametros):
+        bloque de instrucciones
+
+    Las funciones pueden ser de cuatro tipos:
+
+    I. Función que no recibe parámetros y no regresa valor.
+    II. Función que no recibe parámetros y regresa valor.
+    III. Función que recibe parámetros y no regresa valor.
+    IV. Función que recibe parámetros y regresa valor.
+"""
