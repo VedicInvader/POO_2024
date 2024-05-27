913bfbc831de0f397b5bb201c3f3827178dfa8f4
VedicInvader <169110838+VedicInvader@users.noreply.github.com>
2024-05-27 11:29:58 -0600

    no message

--- parcial1/7_funciones/funciones.py
+++ parcial1/7_funciones/funciones.py
@@ -14,3 +14,24 @@
     III. Función que recibe parámetros y no regresa valor.
     IV. Función que recibe parámetros y regresa valor.
 """
+
+#Ejemplo 1: crear una función para imprimir nombres de personas
+#Función que no recibe parámetros y no regresa valor
+
+def solicitarNombre():
+    nombre = input("Ingrese su nombre completo: ")
+
+solicitarNombre()
+
+#Ejemplo 2: sumar dos números
+
+def suma():
+    num1 = int(input("Numero 1: "))
+    num2 = int(input("Numero 2: "))
+    suma = num1 + num2
+    print(f"La suma de {num1} y {num2} es: {suma}")
+
+suma()
+
+#Ejemplo 3: sumar dos números
+#Función que no recibe parámetros y regresa valor
