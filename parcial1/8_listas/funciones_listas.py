6295a7c865279f0903795ade56e33a7a3b63a9da
VedicInvader <169110838+VedicInvader@users.noreply.github.com>
2024-06-07 10:20:12 -0600

    no message

---
+++ parcial1/8_listas/funciones_listas.py
@@ -0,0 +1,44 @@
+paises = ["Islandia", "Noruega", "Suecia", "Finlandia"]
+numeros = [20, 40, 3.1415, 14.88]
+aleatorios = ["Rigveda", True, 14, 9.2]
+
+#Ordenar listas
+
+# print(paises)
+# paises.sort() #Ordenar alfabeticamente
+# print(paises)
+
+#Agregar elementos
+
+# print(numeros)
+# numeros.insert(len(numeros), 100)
+# print(numeros)
+# numeros.append(100)
+
+#Eliminar elementos
+
+# print(numeros)
+# numeros.pop(2) #En "pop" se pone la posicion a remover
+# print(numeros)
+# numeros.remove(20)
+
+#Buscar dentro de la lista un dato o valor
+# buscar = "Suiza" in paises
+# print(buscar)
+
+#Dar la vuelta (ordenar en reversa)
+# print(aleatorios)
+# aleatorios.reverse()
+# print(aleatorios)
+
+#Concatenar listas (unir listas)
+# print(paises)
+# paises.extend(numeros)
+# print(paises)
+
+#Vaciar una lista
+print(aleatorios)
+aleatorios.clear()
+print(aleatorios)
+
+
